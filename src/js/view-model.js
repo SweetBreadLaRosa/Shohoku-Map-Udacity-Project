4878fc018edcedddfd557d28f4e2d583dc32f536
John LaRosa <john.larosa@apexlearning.com>
2017-12-08 01:20:21 -0800

    finally got data from four sqaure and adding it to the infoWindow

--- src/js/view-model.js
+++ src/js/view-model.js
@@ -11,6 +11,7 @@
         this.locationsList = ko.observableArray(Model.hoopLocations.slice(0));
         this.filterSearchText = ko.observable('');
         this.onLocationClick = onLocationClick;
+        this.locationAddress = '';
 
         // First this should initialize the whole app
         function initializeApp() {
@@ -83,8 +84,17 @@
 
             // add title of court to content string and render in UI
             // todo: maybe we should move this to the view
-            infoWindow.setContent('<div>' + marker.title + '</div>');
-            infoWindow.open(View.googleMapObject, marker);
+
+            // lets try adding the address we get from 4 square
+
+            console.log('this should be different everytime', marker.locationData.location);
+            get4SquareLocationData(
+                marker.locationData.location,
+                marker.locationData.title,
+                function(data) {
+                    infoWindow.setContent('<div>' + data.name + '</div><br><div>' + 'Address: ' + data.location.address + '</div>');
+                    infoWindow.open(View.googleMapObject, marker);
+            });
 
             if (infoWindow.marker) {
                 infoWindow.marker.setAnimation(null);
@@ -100,6 +110,34 @@
                     return;
                 }
             }
+        }
+
+        // { lat: 47.6173, lng: -122.3195 } testing
+
+        // foursquare time!
+        function get4SquareLocationData(location, name, callback) {
+            $.ajax({
+                url: buildUrlForAjaxReq(location, name),
+                dataType: 'json',
+                async: true,
+                success: function (data) {
+                    console.log('success', data.response.venues[0]);
+                    callback(data.response.venues[0]);
+                },
+                error: function (e) {
+                    console.log('error in ajax req', JSON.stringify(e));
+                    callback = null;
+                }
+            });
+        }
+
+        function buildUrlForAjaxReq(latlng, name) {
+            return 'https://api.foursquare.com/v2/venues/search?' +
+            'll=' + latlng.lat + ',' + latlng.lng +
+            '&intent=match&name=' + name +
+            '&client_id=JX12DKPHUV2M2MFYK14QO50JMNLWCDPLHMO4EAQ3YON5EZSS' +
+            '&client_secret=JXJBAZ4AOCPWRPCY4JNSUCTXM4A54ZLUI0I0Y1B0BV04H3HI' +
+            '&v=20170801&m=foursquare'
         }
 
         // created vm class in order to invoke when loaded
