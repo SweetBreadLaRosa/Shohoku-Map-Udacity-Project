f7825ed1b9cf13a12e564918405fdbc3bc4a54c4
John LaRosa <john.larosa@apexlearning.com>
2017-12-08 15:17:57 -0800

    trying to make responsive

--- src/js/view.js
+++ src/js/view.js
@@ -68,13 +68,13 @@
 
         // grab data from 4square and update the infoWindow
         updateInfoWindow: function (marker, data) {
+
             // default to just title form the Model
             if (null) {
                 this.infoWindow.setContent('<div>' + marker.locationData.title + '</div>');
             }
 
             // use formatted address array instead of just the address property from the data object
-
             var addressData = data.location.formattedAddress;
 
             this.infoWindow.setContent('<div>' +
